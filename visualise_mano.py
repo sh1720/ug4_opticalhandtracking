e28df684967513b89c90bdc2e601fba2d446d219
sh1720 <sh1720@ic.ac.uk>
2025-02-20 12:56:41 +0000

    requirements.txt

--- visualise_mano.py
+++ visualise_mano.py
@@ -8,7 +8,7 @@ import pickle as pickle
 from tqdm import tqdm
 
 MANO_MODEL_PATH = "ug4_opticalhandtracking/mano_models/MANO_RIGHT.pkl"
-pkl_file_path = "D:/FYP Datasets/HO3D_v2/train/ABF10/meta/0000.pkl"
+pkl_file_path = "D:/FYP Datasets/HO3D_v2/train/ABF10/meta/0144.pkl"
 
 # Load the .pkl file with a progress bar
 with open(pkl_file_path, 'rb') as f:
